e85b7c1bd88a0664889369f61962f3c25d13cd20
Paul Mooney <pmooney@cpan.org>
2009-10-30 12:10:20 +0000

    Import of PMOONEY/Catalyst-Engine-Stomp-0.08 from CPAN.
    gitpan-cpan-distribution: Catalyst-Engine-Stomp
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         PMOONEY/Catalyst-Engine-Stomp-0.08.tar.gz
    gitpan-cpan-author:       PMOONEY
    gitpan-cpan-maturity:     released

--- t/lib/StompTestApp/Controller/TestController.pm
+++ t/lib/StompTestApp/Controller/TestController.pm
@@ -2,8 +2,34 @@ package # Hide from PAUSE
   StompTestApp::Controller::TestController;
 use Moose;
 use namespace::autoclean;
+use Encode;
 
 BEGIN { extends 'Catalyst::Controller::MessageDriven' };
+
+=head1 testutf8()
+
+Return a datastructure containing a string that has a character that
+needs to be encoded into utf-8 when sent via STOMP
+
+=cut
+
+sub testutf8 : Local {
+    my ($self, $c, $request) = @_;
+
+    my $new_string   = "Maria van Bourgondiëlaan";
+    Encode::_utf8_on($new_string); # this is now how we get things from XML::Compile which deals with XML in utf-8 encoding
+
+    # Reply with a minimal response message
+    my $response = {
+        type   => 'testutf8_response',
+        struct => [
+            { foo         => 'bar' },
+            { new_string  => $new_string },
+        ],
+    };
+    $c->stash->{response} = $response;
+}
+
 
 sub testaction : Local {
     my ($self, $c, $request) = @_;
@@ -16,6 +42,12 @@ sub testaction : Local {
 sub badaction : Local {
     my ($self, $c, $request) = @_;
     die "oh noes";
+}
+
+sub throwerror : Local {
+    my ($self, $c, $request) = @_;
+    my $obj = bless {error => 'oh noes'}, 'StompTestApp::Error';
+    die $obj;
 }
 
 sub ping : Local {
